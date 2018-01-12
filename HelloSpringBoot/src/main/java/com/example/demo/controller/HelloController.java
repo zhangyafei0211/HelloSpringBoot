81701c1ec2ec4c6f32d3023a5755f536b5b97c9b
zhangyafei0211 <zhangyafei0211@163.com>
2018-01-12 16:19:30 +0800

    第一个项目创建

---
+++ HelloSpringBoot/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String say(){
+		System.out.println("this is "+ this.getClass());
+		return "Hello SpringBoot!";
+	}
+}
